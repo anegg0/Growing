34ced01398fc7b3e768c77856e8b9c1e3199c9b6
anegg0 <gael.offers@protonmail.com>
2020-04-16 14:34:32 -0700

    with faulty property

---
+++ Growing/Growing/Shared/Models/Tray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Growing.Shared.Models
+{
+    public class Tray
+    {
+        public int TrayId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Department { get; set; } 
+        [Required]
+        public string City { get; set; }
+        //public System.DateTime SowingDate { get; set; }
+        public string SowingDate   // property
+        {
+            get { return SowingDate; }   // get method
+            set { SowingDate = System.DateTime.Now.ToLongDateString(); }  // set method
+        }
+    }
+}
