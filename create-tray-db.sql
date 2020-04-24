5d7f819e09e3803d0082f9a30f1e2c5a49db4356
anegg0 <gael.blanchemain@protonmail.com>
2020-04-24 09:24:42 -0700

    attempt from linux

--- create-tray-db.sql
+++ create-tray-db.sql
@@ -13,3 +13,15 @@ CREATE TABLE `tblTray` (
   PRIMARY KEY (`TrayId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 INSERT INTO `tblTray` VALUES (1,'nigo','marseille','loiret','fluid','2019-01-30');
+
+CREATE TABLE `tblTrayType` (
+  `TrayTypeId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(12) NOT NULL,
+  `handle` varchar(11) NOT NULL,
+  `length` varchar(11) NOT NULL,
+  `width` varchar(11) NOT NULL,
+  `height` varchar(11) NOT NULL,
+  `notes` varchar(150) NOT NULL,
+  PRIMARY KEY (`TrayTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `tblTrayType` VALUES (1,'Long Wook','LG','1','3','9','pouet');
